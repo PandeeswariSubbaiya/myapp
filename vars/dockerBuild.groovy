37bf66be031ae8757ab801b57c2330a345b6674b
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-13 14:01:10 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,12 +2,13 @@ def call(String project, String hubUser) {
     sh "docker image build -t ${hubUser}/${project} ."
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
-    withCredentials([usernamePassword(
-            credentialsId: "dockerPass",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
+    //withCredentials([usernamePassword(
+            //credentialsId: "dockerlibrary",
+            //usernameVariable: "USER",
+            //passwordVariable: "PASS"
+   // )]) 
+     withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
+        sh "docker login -u pandeeswari1814 -p ${dockerPassword}"
     }
     sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"
