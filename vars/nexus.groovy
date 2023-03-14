d47982e3d9fcde9dba1a1b2bb3a4ea4abfe9150d
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-14 17:31:29 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,8 +1,3 @@
 def call(String groupId, String artifactId, String version, String packaging, String file, String repositoryUrl, String repositoryUsername, String repositoryPassword) {
-    def nexusUrl = repositoryUrl + "/repository"
-    def nexusAuth = "${repositoryUsername}:${repositoryPassword}"
-    def nexusArtifactPath = "${groupId.replaceAll("\\.", "/")}/${artifactId}/${version}/${artifactId}-${version}.${packaging}"
-    def nexusUploadUrl = "${nexusUrl}/maven-releases/${nexusArtifactPath}"
-    
-    sh "curl --fail --silent --show-error --upload-file ${file} -u ${nexusAuth} ${nexusUploadUrl}"
+    new com.mycompany.jenkins.sharedlibrary.Nexus().upload(groupId, artifactId, version, packaging, file, repositoryUrl, repositoryUsername, repositoryPassword)
 }
