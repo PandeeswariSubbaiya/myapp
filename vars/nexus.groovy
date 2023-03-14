1c6c9241e2027ea05adb7868fefee1ebb08cdfba
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-14 18:07:35 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,5 +1,19 @@
-def publishToNexus(String repositoryUrl, String groupId, String artifactId, String version, File artifactFile) {
-  def nexus = new NexusRepository(repositoryUrl)
-  nexus.authenticate('username', 'password')
-  nexus.uploadMaven(groupId, artifactId, version, artifactFile)
+def call(String groupId, String artifactId, String version, String packaging, String repositoryUrl, String username, String password, String file) {
+    def server = new org.apache.maven.artifact.ant.RemoteRepository()
+    server.setUrl(repositoryUrl)
+    server.setId("nexus")
+    server.setUsername(username)
+    server.setPassword(password)
+    def pom = new XmlSlurper().parseText("""
+            4.0.0
+            ${groupId}
+            ${artifactId}
+            ${version}
+            ${packaging}
+    """)
+    def deployer = new org.apache.maven.artifact.deployer.DefaultArtifactDeployer()
+    def artifact = new org.apache.maven.artifact.DefaultArtifact(groupId, artifactId, version, "compile", packaging, null, new org.apache.maven.artifact.handler.DefaultArtifactHandler(packaging))
+    artifact.setFile(new java.io.File(file))
+    deployer.setRepository(server)
+    deployer.deploy(artifact, pom, server, null)
 }
