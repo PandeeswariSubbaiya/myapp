57fafb5b56642f44599989617e0532abcf9e28f9
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 14:36:51 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,11 +1,3 @@
-//def call(Map config) {
-        //sh """
-            //terraform init -input=false ${config.tf_init_args}
-            //terraform plan -input=false ${config.tf_plan_args}
-            //terraform apply -input=false ${config.tf_apply_args}
-        //"""
-    //}
-//}
 def init(Map config) {
     sh """
     mkdir -p terraform
