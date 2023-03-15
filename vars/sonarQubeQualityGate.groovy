7b6b2dfdacb5671f427d5c8b085ecb13a334c8af
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 12:25:48 +0530

    Update sonarQubeQualityGate.groovy

--- vars/sonarQubeQualityGate.groovy
+++ vars/sonarQubeQualityGate.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
-    def qualityGateUrl = "${config.sonarqube_url}/api/qualitygates/project_status?projectName=${config.project_name}"
+    def qualityGateUrl = "${config.sonarqube_url}"
     def qualityGateResult = sh (
-        script: "curl -s -u ${config.sonarqube_token}: ${qualityGateUrl} | jq -r .projectStatus.status",
+        script: "curl -s -u ${config.sonarqube_token}: ${qualityGateUrl}" ,
         returnStdout: true
     ).trim()
     
