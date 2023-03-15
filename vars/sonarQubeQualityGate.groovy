8074a73f66b22d617a0227dedc14b53f12a50d30
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 12:21:06 +0530

    Update sonarQubeQualityGate.groovy

--- vars/sonarQubeQualityGate.groovy
+++ vars/sonarQubeQualityGate.groovy
@@ -8,7 +8,4 @@ def call(Map config) {
     if (qualityGateResult != 'OK') {
         error "Quality gate failed: ${qualityGateResult}"
     }
-    
-    // Quality gate passed, do something
-    echo "Quality gate passed!"
 }
