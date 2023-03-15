3f0a0c8f1452eafc21b43061c006fc38b6fa8246
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 11:58:50 +0530

    Create sonarQubeQualityGate.groovy

---
+++ vars/sonarQubeQualityGate.groovy
@@ -0,0 +1,11 @@
+def call(Map config) {
+    def qualityGateUrl = "${config.sonarqube_url}/api/qualitygates/project_status?projectKey=${config.project_key}"
+    def qualityGateResult = sh (
+        script: "curl -s -u ${config.sonarqube_token}: ${qualityGateUrl} | jq -r .projectStatus.status",
+        returnStdout: true
+    ).trim()
+    
+    if (qualityGateResult != 'OK') {
+        error "Quality gate failed: ${qualityGateResult}"
+    }
+}
