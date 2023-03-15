23df82b2769259d7ae61e063d74763900b0361e2
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 11:12:43 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,5 +1,9 @@
-def call(Map config) {
-    sh "terraform init -backend-config=${config.backend_config_file}"
-    sh "terraform plan -var-file=${config.var_file}"
-    sh "terraform apply -var-file=${config.var_file}"
+def init(Map config) {
+    sh """
+    mkdir -p terraform
+    cp ${config.ec2_file} terraform/ec2.tf
+    cp ${config.provider_file} terraform/provider.tf
+    cd terraform
+    terraform init
+    """
 }
