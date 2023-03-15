88efcf455a320b30aef5369cefdb209ecc2328d4
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 09:45:04 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,11 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String imageName){
-    echo "Building Docker image..."
-    //withCredentials([usernamePassword(credentialsId: "dockerhub-credentials", usernameVariable: "username", passwordVariable: "password")]){
-      withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
-        sh "docker build -t $imageName ."
-        sh "echo $password | docker login -u $username --password-stdin"
-        sh "docker push $imageName"
-    }
+    sh "docker  build -t ${imageTag} . "
 }
