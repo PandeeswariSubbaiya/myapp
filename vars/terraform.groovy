e971425d790c1a597d8038a926c717030ad28236
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 13:58:39 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,8 +1,11 @@
 def call(Map config) {
         sh """
-            terraform init -input=false ${config.tf_init_args}
-            terraform plan -input=false ${config.tf_plan_args}
-            terraform apply -input=false ${config.tf_apply_args}
+            //terraform init -input=false ${config.tf_init_args}
+            //terraform plan -input=false ${config.tf_plan_args}
+            //terraform apply -input=false ${config.tf_apply_args}
+            terraform init -reconfigure ${config.tf_init_args}
+            terraform plan -lock=false ${config.tf_plan_args}
+            terraform apply -lock=false -auto-approve ${config.tf_apply_args}
         """
     }
 }
