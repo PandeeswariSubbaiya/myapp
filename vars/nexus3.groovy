25c6c105cf6c946f4dddab3ec9010db371d904dd
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 09:40:19 +0530

    Delete nexus3.groovy

--- vars/nexus3.groovy
+++
@@ -1,5 +0,0 @@
-def publishToNexus(String repositoryUrl, String groupId, String artifactId, String version, File artifactFile) {
-  def nexus = new NexusRepository(repositoryUrl)
-  nexus.authenticate('username', 'password')
-  nexus.uploadMaven(groupId, artifactId, version, artifactFile)
-}
