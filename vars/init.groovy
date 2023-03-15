b1332ca02ae1472657e191f1d2886f0b6f64e298
PandeeswariSubbaiya <124767670+PandeeswariSubbaiya@users.noreply.github.com>
2023-03-15 14:37:08 +0530

    Delete init.groovy

--- vars/init.groovy
+++
@@ -1,9 +0,0 @@
-def init(Map config) {
-    sh """
-    mkdir -p terraform
-    cp ${config.ec2_file} terraform/ec2.tf
-    cp ${config.provider_file} terraform/provider.tf
-    cd terraform
-    terraform init
-    """
-}
